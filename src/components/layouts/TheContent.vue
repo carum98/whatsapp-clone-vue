6830296df0e9c609817ba9e1960b48b4de41f98b
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-06-19 14:42:11 -0600

    Landing banner

--- src/components/layouts/TheContent.vue
+++ src/components/layouts/TheContent.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useChat } from '../../composables/useChat';
 import TheConversation from '../conversation/TheConversation.vue';
+import LadingBanner from '../../assets/LadingBanner.vue';
 const { chat } = useChat();
 </script>
 
 <template>
-	<section id="content">
+	<section id="content" :class="{ banner: chat === null }">
 		<TheConversation v-if="chat" :chat="chat" :key="chat.id" />
+
+		<div v-else>
+			<LadingBanner />
+			<h1>WhatsApp Web Clone</h1>
+		</div>
 	</section>
 </template>
